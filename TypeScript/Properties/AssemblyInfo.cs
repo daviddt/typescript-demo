65186159e0d805ff378044598811e6e9764e6f55
unknown <Sander.Sietses@E1750.efocus.local>
2015-05-01 14:11:46 +0200

    added visual studio project

--- TypeScript/Properties/AssemblyInfo.cs
+++ TypeScript/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1fc315e6-b384-46fd-843c-d11a9f1ac36a")]
+[assembly: Guid("67a52647-5cd1-4979-ae29-27edd0cbff10")]
 
 // Version information for an assembly consists of the following four values:
 //
